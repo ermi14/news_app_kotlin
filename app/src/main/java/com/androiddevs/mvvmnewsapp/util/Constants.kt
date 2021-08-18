fa5e98776533faa6fddd1a1fda64b8eaf50b5f4a
ermi14 <ermikasa@gmail.com>
2021-08-18 16:51:12 +0300

    local db done

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "976793ba230043409984d2d6737ef8e4"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
